81d9163def93b40b812bedd61dc503258c5b1daf
Jaw G Chin <1679673306@qq.com>
2022-01-26 16:55:00 +0800

    master

--- hellow_world.py
+++ hellow_world.py
@@ -1,12 +1,3 @@
 print('hello')
 print(5)
 print(9.556)
-
-
-data = open('F:\Program Files (x86)\python_study/test.txt', 'a+')
-print('test', file=data)
-data.close()
-
-print('45', '哈哈哈哈哈', 'more')
-
-print('this is mac done')
