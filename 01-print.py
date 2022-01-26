9a60386e67e751388fba993072a47502397bbe33
zhouzhicheng61 <1679673306@qq.com>
2022-01-26 20:51:32 +0800

    master

--- 01-print.py
+++ 01-print.py
@@ -6,3 +6,12 @@ print(3+5)
 print(3*5)
 print(3**4444)
 print('大幅度'+'奥术大师多')
+print('gasldfi')
+print('------1231231------')
+temp=input('猜数字')
+guess=int(temp)
+if guess==8:
+    print('牛逼')
+else:
+    print('垃圾，我想的是8')
+print('gg')
