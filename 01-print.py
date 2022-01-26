ad6651bbc6a58205b1042efa8c4c817059f28516
zhouzhicheng61 <1679673306@qq.com>
2022-01-26 21:39:27 +0800

    master

--- 01-print.py
+++ 01-print.py
@@ -2,15 +2,15 @@
 print('周治成')
 print(5)
 print(9.556)
-print(3+5)
-print(3*5)
+print(3 + 5)
+print(3 * 5)
 print(3**4444)
-print('大幅度'+'奥术大师多')
+print('大幅度' + '奥术大师多')
 print('gasldfi')
 print('------1231231------')
-temp=input('猜数字')
-guess=int(temp)
-if guess==8:
+temp = input('猜数字')
+guess = int(temp)
+if guess ==8:
     print('牛逼')
 else:
     print('垃圾，我想的是8')
