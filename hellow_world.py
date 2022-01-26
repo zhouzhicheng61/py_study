3042e9a4f1c51a03587aaac1066386c1d301d76c
zhouzhicheng61 <1679673306@qq.com>
2022-01-26 16:00:28 +0800

    master

---
+++ hellow_world.py
@@ -0,0 +1,11 @@
+print('hello')
+print(5)
+print(9.556)
+
+# 将数据输出到文件中
+# 注意：1、文件路径；2、file=
+data = open('F:\Program Files (x86)\python_study/test.txt', 'a+')
+print('test', file=data)
+data.close()
+
+print('233333', '哈哈哈哈哈', 'more')
