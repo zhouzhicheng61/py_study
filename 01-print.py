2cc24e1ed5e48604900bbfe7e41a10b965bf4fe3
zhouzhicheng61 <1679673306@qq.com>
2022-01-26 21:55:49 +0800

    master

--- 01-print.py
+++ 01-print.py
@@ -10,7 +10,7 @@ print('gasldfi')
 print('------1231231------')
 temp = input('猜数字')
 guess = int(temp)
-if guess ==8:
+if guess == 8:
     print('牛逼')
 else:
     print('垃圾，我想的是8')
--- 02-variable.py
+++ 02-variable.py
@@ -1,13 +1,13 @@
 # -*-coding:utf-8-*-
-teacher='小甲鱼'
+teacher = '小甲鱼'
 print(teacher)
-teacher='老甲鱼'
+teacher = '老甲鱼'
 print(teacher)
-first=3
-second=5
-third=first+second
+first = 3
+second = 5
+third = first + second
 print(first, second, third)
-myteacher='小甲鱼'
-yourteacher='黑夜'
-ourteacher=myteacher+yourteacher
+myteacher = '小甲鱼'
+yourteacher = '黑夜'
+ourteacher = myteacher + yourteacher
 print(ourteacher)
