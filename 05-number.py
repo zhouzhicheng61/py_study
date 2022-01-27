323582fb9f1c8ad43365fa22097a459fa885b5b4
Jaw G Chin <1679673306@qq.com>
2022-01-27 10:57:49 +0800

    master

---
+++ 05-number.py
@@ -0,0 +1,13 @@
+# -*-coding:utf-8-*-
+#数字型：1、整型；2、浮点型；3、复数
+a = 1
+b = 0.2
+c = 0.1
+print(b + c)
+i = 0
+while i < 1:
+    i = i + 0.1
+    print(i)
+print(0.00005)  #5e-05 表示5乘以10的负五次方
+x = 1 + 2j
+print(x.real, x.imag)
