e795711c302e52398aa5013fa75fbbd0cd2bfa5c
zhouzhicheng61 <1679673306@qq.com>
2022-01-28 17:28:41 +0800

    master

---
+++ 10-list.py
@@ -0,0 +1,10 @@
+mylist = [1, 2, 3, 4, 5, '上山打老虎']
+for each in mylist:
+    print(each)
+print(mylist[3])
+print(mylist[-1])
+print(mylist[1:3])
+print(mylist[:3])
+print(mylist[3:])
+print(mylist[:])
+print(mylist[::2])
