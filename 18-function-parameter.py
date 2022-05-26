110d4aff691c773bd1b1b9066f7ff1a79b957257
zhouzhicheng61 <1679673306@qq.com>
2022-05-26 10:57:51 +0800

    master

--- 18-function-parameter.py
+++ 18-function-parameter.py
@@ -3,3 +3,20 @@ def func1(a, b, c):
 
 
 print(func1('u', 'hit', 'me'))
+
+
+# 设置参数默认值
+def func2(a, b="zzc", c="香蕉"):
+    return "".join((b, a, c))
+
+
+print(func2("吃", "abc"))
+
+
+#收集参数
+def myfunc(*args):
+    print("有{}个参数".format(len(args)))
+    print("第二个参数是：{}".format(args[1]))
+
+
+myfunc(1, 2, 3, 4, 5, 6)
